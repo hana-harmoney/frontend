68fb6b4d281f2d30f5dce252bd42f8c0257584c1
VarGun <heegun46900@gmail.com>
2025-08-22 15:18:13 +0900

    [#4] style: 배지 컴포넌트 및 색상 추가

---
+++ src/components/jobs/card.tsx
@@ -0,0 +1,7 @@
+const Card = () => {
+  return (
+    <div className="border-teduri flex flex-col rounded-2xl">
+      <div className="border-bottom border-teduri flex items-center justify-center"></div>
+    </div>
+  );
+};
