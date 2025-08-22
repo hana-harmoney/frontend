f1d56e1b18f8f11661973df9deff2872fb724753
jhpark0888 <jhpark0888@naver.com>
2025-08-22 16:07:56 +0900

    [#4] fix: svg 이미지 안 보이는 오류 해결

--- next.config.ts
+++ next.config.ts
@@ -9,17 +9,10 @@ const nextConfig: NextConfig = {
             loader: '@svgr/webpack',
             options: {
               icon: true,
-              svgo: true,
-              svgoConfig: {
-                plugins: [
-                  { name: 'removeViewBox', active: false },
-                  { name: 'convertColors', params: { currentColor: true } },
-                ],
-              },
             },
           },
         ],
-        as: '*.js', // SVG -> JS(리액트 컴포넌트)로 다뤄라
+        as: '*.ts',
       },
     },
   },
