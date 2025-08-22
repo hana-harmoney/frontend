a69eaf1efd68d72e9c3ca0dc05cd1a03df87ee91
jhpark0888 <jhpark0888@naver.com>
2025-08-22 15:22:24 +0900

    [#2] feat: svgr webpack 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              icon: true,
+              svgo: true,
+              svgoConfig: {
+                plugins: [
+                  { name: 'removeViewBox', active: false },
+                  { name: 'convertColors', params: { currentColor: true } },
+                ],
+              },
+            },
+          },
+        ],
+        as: '*.js', // SVG -> JS(리액트 컴포넌트)로 다뤄라
+      },
+    },
+  },
 };
 
 export default nextConfig;
