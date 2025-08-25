7163a7dbda9204765aa9a71fe224c072cec63d4b
jhpark0888 <jhpark0888@naver.com>
2025-08-25 18:57:33 +0900

    [#14] feat: 채팅 텍스트필드 UI 구현

---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { RefObject, useRef, useState } from 'react';
+import Send from '@/assets/icons/send.svg';
+import Microphone from '@/assets/icons/microphone.svg';
+import { InputWithIcon } from '../common/inputWithIcon';
+
+type Props = {
+  inputRef?: RefObject<HTMLInputElement | null>;
+  onSend: (text: string) => void;
+};
+
+export default function ChatInput({ inputRef, onSend }: Props) {
+  const [text, setText] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    handleSend();
+  };
+
+  const handleSend = () => {
+    if (!text.trim()) return;
+
+    onSend(text);
+    setText('');
+  };
+
+  const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="frame-container flex w-full gap-1 p-2.5"
+    >
+      <InputWithIcon
+        className="text-text h-12 flex-grow rounded-lg text-xl"
+        ref={inputRef}
+        value={text}
+        placeholder="메세지를 입력해주세요..."
+        icon={<Microphone className="size-8" />}
+        onChange={handleChangeText}
+      />
+      <button type="submit" onClick={handleSend}>
+        <Send className="size-10" />
+      </button>
+    </form>
+  );
+}
