2e9afff1d17c2b4973d3560c5533afc1a6a8bcf2
jhpark0888 <jhpark0888@naver.com>
2025-08-25 16:02:49 +0900

    [#14] feat: 채팅 헤더 액션 버튼 구현

---
+++ src/components/chat/ReportButton.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Warning from '@/assets/icons/warning.svg';
+
+export default function ReportButton() {
+  return (
+    <button className="cursor-pointer" type="button" onClick={() => {}}>
+      <Warning className="size-8" />
+    </button>
+  );
+}
