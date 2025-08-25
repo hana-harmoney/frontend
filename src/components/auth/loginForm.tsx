1298940d4014d2c8230b50803dbca01b31894bf5
jhpark0888 <jhpark0888@naver.com>
2025-08-25 11:17:01 +0900

    [#9] fix: 로그인 폼 gap 조정

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -44,36 +44,37 @@ export default function LoginForm() {
   }
 
   return (
-    <form
-      onSubmit={handleLogin}
-      className="flex w-full flex-col items-center justify-center gap-5"
-    >
-      <InputWithLabel
-        label={'아이디'}
-        placeholder="영문, 숫자 조합 6자 이상"
-        value={id}
-        onChange={handleChangeId}
-      />
-      <InputWithLabel
-        label={'비밀번호'}
-        type="password"
-        placeholder="비밀번호를 입력하세요"
-        value={password}
-        onChange={handleChangePassword}
-      />
-      <Button
-        className="bg-hana-green h-[52px] w-full rounded-sm text-2xl"
-        type="submit"
-        disabled={loading}
+    <div className="flex w-full flex-col items-center gap-2">
+      <form
+        onSubmit={handleLogin}
+        className="flex w-full flex-col items-center justify-center gap-5"
       >
-        {loading ? '로그인 중...' : '로그인'}
-      </Button>
+        <InputWithLabel
+          label={'아이디'}
+          placeholder="영문, 숫자 조합 6자 이상"
+          value={id}
+          onChange={handleChangeId}
+        />
+        <InputWithLabel
+          label={'비밀번호'}
+          type="password"
+          placeholder="비밀번호를 입력하세요"
+          value={password}
+          onChange={handleChangePassword}
+        />
+        <Button
+          className="bg-hana-green h-[52px] w-full rounded-sm text-2xl"
+          type="submit"
+          disabled={loading}
+        >
+          {loading ? '로그인 중...' : '로그인'}
+        </Button>
 
-      {err && <p className="text-sm text-red-600">{err}</p>}
-
-      <Link href={'/auth/signup'}>
+        {err && <p className="text-sm text-red-600">{err}</p>}
+      </form>
+      <Link className="" href={'/auth/signup'}>
         <p className="text-text-secondary text-xl">회원가입 하러가기</p>
       </Link>
-    </form>
+    </div>
   );
 }
