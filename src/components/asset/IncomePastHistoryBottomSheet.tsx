96ab9878081f170a955f6d6b0b412cadf1eda340
jhpark0888 <jhpark0888@naver.com>
2025-09-04 10:25:49 +0900

    [#97] feat: 하모니 수익 지난 내역 API 연동

--- src/components/asset/IncomePastHistoryBottomSheet.tsx
+++ src/components/asset/IncomePastHistoryBottomSheet.tsx
@@ -2,41 +2,30 @@ import { cn } from '@/lib/utils';
 import BottomSheet from '../common/bottomSheet';
 import BarChart from '@/assets/icons/bar_chart.svg';
 import IncomePastHistoryChart from './IncomePastHistoryChart';
+import { useEffect, useState } from 'react';
+import { fetchPastHarmoneyIncome } from '@/lib/api/report';
 
 type Props = {
   open: boolean;
   onClose: () => void;
 };
 
-const datas = [
-  {
-    month: 6,
-    income: 100000,
-  },
-  {
-    month: 7,
-    income: 300000,
-  },
-  {
-    month: 8,
-    income: 210000,
-  },
-  {
-    month: 9,
-    income: 200000,
-  },
-];
-
 type IncomeData = {
+  date: Date;
   month: number;
   income: number;
+  height: number;
 };
 
 export default function IncomePastHistoryBottomSheet({ open, onClose }: Props) {
-  const isPlus = true;
-  const calculatedData = getHeights(datas);
+  const [datas, setDatas] = useState<IncomeData[]>([]);
+  const [incomeDiffFromPrevMonth, setIncomeDiffFromPrevMonth] = useState(0);
 
-  function getHeights(data: IncomeData[], minHeight = 24, maxHeight = 80) {
+  function getHeights(
+    data: { date: Date; month: number; income: number }[],
+    minHeight = 24,
+    maxHeight = 80,
+  ) {
     const incomes = data.map((d) => d.income);
     const minVal = Math.min(...incomes);
     const maxVal = Math.max(...incomes);
@@ -53,6 +42,33 @@ export default function IncomePastHistoryBottomSheet({ open, onClose }: Props) {
     });
   }
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetchPastHarmoneyIncome();
+
+        const parsed = res
+          .map((income) => {
+            const date = new Date(income.month);
+            return {
+              date: date,
+              month: date.getMonth() + 1,
+              income: income.monthlyAmount,
+            };
+          })
+          .sort((a, b) => a.date.getTime() - b.date.getTime());
+
+        const incomeThis = parsed[parsed.length - 1].income;
+        const incomePrev = parsed[parsed.length - 2].income;
+
+        setIncomeDiffFromPrevMonth(incomeThis - incomePrev);
+        setDatas(getHeights(parsed));
+      } catch (e) {
+      } finally {
+      }
+    })();
+  }, []);
+
   return (
     <BottomSheet open={open} onClose={onClose} contentClassName="h-fit">
       <div className="w-full overflow-y-auto overscroll-contain px-5 pb-25">
@@ -63,14 +79,18 @@ export default function IncomePastHistoryBottomSheet({ open, onClose }: Props) {
         </div>
         <p className="text-gray my-5 text-center text-xl font-medium">
           전월 대비 이번 달 수입이
-          <span className={cn(isPlus ? 'text-hana-green' : 'text-hana-red')}>
+          <span
+            className={cn(
+              incomeDiffFromPrevMonth > 0 ? 'text-hana-green' : 'text-hana-red',
+            )}
+          >
             {' '}
-            5만원{' '}
+            {incomeDiffFromPrevMonth / 10000}만원{' '}
           </span>
-          {isPlus ? '늘었어요!' : '줄었어요!'}
+          {incomeDiffFromPrevMonth > 0 ? '늘었어요!' : '줄었어요!'}
         </p>
         <div className="bg-gray-3 gap-2.5 rounded-md px-2.5 pt-6 pb-2.5">
-          <IncomePastHistoryChart datas={calculatedData} />
+          <IncomePastHistoryChart datas={datas} />
           <p className="text-subtitle pt-2.5 text-center text-base font-medium">
             월별 하모니 수입
           </p>
